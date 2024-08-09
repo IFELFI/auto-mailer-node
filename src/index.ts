4afd86aee637f349f845aceaf52d21d61e7f9b99
sauce-git <sauce@mandacode.com>
2024-08-10 00:52:16 +0900

    chore: Add .env and dist to .gitignore, create nodemon.json, res.d.ts, logger.ts, error.ts, Dockerfile, and update tsconfig.json and package.json

--- src/index.ts
+++ src/index.ts
@@ -1,41 +1,57 @@
 import net from 'net';
 import 'dotenv/config';
 import config from './config';
-import { BufferData } from './validations/buffer';
+import { BufferData, BufferDataSchema } from './validations/buffer';
 import { sendAuthCode } from './services/authCode.service';
-import { log } from 'console';
+import { logger } from './utils/logger';
+import { validate } from './validations';
+import { generateError } from './utils/error';
 
 const server = net.createServer((socket) => {
   socket.setEncoding('utf-8');
 
-  socket.on('data', (buffer: BufferData) => {
-    // if (buffer.cmd === 'echo') {
-    //   sendAuthCode(buffer.data);
-    // } else {
-    //   socket.write(buffer.data);
-    //   socket.write('Unknown command');
-    // }
-    sendAuthCode({email: 'ifelfi@gmail.com', code: '1234'})
+  socket.on('data', (buffer: Buffer) => {
+    let bufferData: BufferData | undefined
+    try {
+      bufferData = JSON.parse(buffer.toString());
+      validate(BufferDataSchema, bufferData);
+    } catch (error: Error | unknown) {
+      if (error instanceof Error) {
+        const errorMessage = generateError(error.message);
+        socket.write(errorMessage);
+        logger.error(error);
+        return;
+      } else {
+        const errorMessage = generateError('Unknown error');
+        socket.write(errorMessage);
+        logger.error(error);
+        return;
+      }
+    }
+
+    if (bufferData && bufferData.cmd === 'sendAuthCode') {
+      sendAuthCode(bufferData.data);
+    } else {
+      socket.write('Unknown command');
+      logger.error(JSON.stringify(bufferData));
+    }
   });
 
   socket.on('close', () => {
-    console.log('Client disconnected');
+    logger.info('Client disconnected');
   });
 
   socket.on('error', (err) => {
-    console.error(err);
+    logger.error(err);
   });
 
   socket.write('Hello from server');
 });
 
 server.on('error', (err) => {
-  console.error(err);
+  logger.error(err);
 });
 
-// server.listen(config.server.port, config.server.host, () => {
-//   console.log(`Server started on ${process.env.HOST}:${process.env.PORT}`);
-// });
-server.listen(config.server.port, () => {
-  console.log(`Server started on ${process.env.HOST}:${process.env.PORT}`);
+server.listen(config.server.port, config.server.host, () => {
+  logger.info(`Server is listening on ${config.server.host}:${config.server.port}`);
 });
